079663dac8f9189cf8394ae2acf3e6bc8544df16
tatynechka <tatynechka@mail.ru>
2016-09-12 03:14:47 +0300

    Админ. Группы

--- js/common.js
+++ js/common.js
@@ -36,6 +36,46 @@ $(function() {
 	});
 
 	//всплывающие подсказки
-	$('[data-toggle="tooltip"]').tooltip();
+	$(function() {
 
+	  $('.tooltip').each(function() {
+	    var el = $(this);
+	    var title = el.attr('title');
+	    if (title && title != '') {
+	      el.attr('title', '').append('<span>' + title + '</span>');
+	      var width = el.find('span').width();
+	      var height = el.find('span').height();
+	      el.hover(
+	        function() {
+	          el.find('span')
+	            .clearQueue()
+	            .delay(200)
+	            .animate({width: 150}, 200).show(200)
+	            .animate({width: 150}, 200);
+	        },
+	        function() {
+	          el.find('span')
+	            .animate({width: 150}, 150)
+	            .animate({width: 'hide', height: 'hide'}, 150);
+	        }
+	      ).mouseleave(function() {
+	        if (el.children().is(':hidden')) el.find('span').clearQueue();
+	      });
+	    }
+	  });
+
+	});
+
+
+	//кастомный скролл
+	$(window).on("load",function(){
+	    $(".table-wrapper").mCustomScrollbar({
+	    	axis:"y",
+	    	scrollButtons:{
+	    		enable: true,
+	    		scrollAmount: "auto",
+	    		scrollType: "stepped",
+	    	}
+	    });
+	});
 });
