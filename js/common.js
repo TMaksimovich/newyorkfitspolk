0c714a97ae725437e8c7c388b7eaba129da2f021
tatynechka <tatynechka@mail.ru>
2016-10-15 22:01:51 +0300

    План питания

--- js/common.js
+++ js/common.js
@@ -51,6 +51,13 @@ $(function() {
 		e.stopPropagation();
 	});
 
+	//табы
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+	    $(this)
+	      .addClass('active').siblings().removeClass('active')
+	      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
 
 	//всплывающие подсказки
 	$(function() {
